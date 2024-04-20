b00d40bea702bbb9d09ddc8775853626829d06ad
Raghav Sharma <raghav.trunks@gmail.com>
2024-04-20 15:54:35 +0530

    first commit

---
+++ PahadiWallpapers/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PahadiWallpapers
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PahadiWallpapers/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PahadiWallpapers
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
